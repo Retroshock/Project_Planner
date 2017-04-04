8f7f374e7e9ac1893d811c285cac51e5f749b7b5
Retroshock <popoviciadrian96@gmail.com>
2017-04-04 18:42:58 +0300

    Small changes

---
+++ app/src/main/java/com/example/adrian/git/Exceptions/InvalidDateException.java
@@ -0,0 +1,28 @@
+package com.example.adrian.git.Exceptions;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import com.example.adrian.git.MainActivity;
+
+/**
+ * Created by Adrian on 03.04.2017.
+ */
+
+public class InvalidDateException extends Exception {
+
+//    InvalidDateException(){
+//        new AlertDialog.Builder()
+//                .setTitle("Invalid Date")
+//                .setMessage ("The date is not cool bro")
+//                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+//                    }
+//                }).show();
+//
+//    }
+}
