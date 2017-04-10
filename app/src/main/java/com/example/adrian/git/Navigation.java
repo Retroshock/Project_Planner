57f004c91a78a2d797bec1e8e7aaa27a0e66c9fb
Banica Mihai <banicamihai96@gmail.com>
2017-04-10 16:16:42 +0300

    Stilizare sign in activity
    Am mai sters si niste chestii care nu foloseau

--- app/src/main/java/com/example/adrian/git/Navigation.java
+++ app/src/main/java/com/example/adrian/git/Navigation.java
@@ -28,13 +28,13 @@ public class Navigation extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), AddActivity.class);
+                startActivity(intent);
             }
         });
 
-        Intent intent = new Intent(this, SignInActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, SignInActivity.class);
+        //startActivity(intent);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
